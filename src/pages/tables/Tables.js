e5b32fa05109d822d39f6ffd403bee49d075e5be
Leonardo Lima <leonardotexeiralima@gmail.com>
2019-05-28 06:34:36 -0300

    Changed table component from 'MUIDataTable' to 'MaterialTable'

--- src/pages/tables/Tables.js
+++ src/pages/tables/Tables.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import MUIDataTable from "mui-datatables";
+import MaterialTable from 'material-table';
 import axios from 'axios';
 
+import {
+  AddBox,
+  Check,
+  ArrowUpward,
+  ChevronLeft,
+  ChevronRight,
+  Clear,
+  DeleteOutline,
+  Edit,
+  FilterList,
+  FirstPage,
+  LastPage,
+  Remove,
+  SaveAlt,
+  Search,
+  ViewColumn,
+} from "@material-ui/icons";
+
 import PageTitle from '../../components/PageTitle';
-import TableToolbarSelect from "./TableToolbarSelect";
+
+const tableIcons = {
+  Add: AddBox,
+  Check: Check,
+  Clear: Clear,
+  Delete: DeleteOutline,
+  DetailPanel: ChevronRight,
+  Edit: Edit,
+  Export: SaveAlt,
+  Filter: FilterList,
+  FirstPage: FirstPage,
+  LastPage: LastPage,
+  NextPage: ChevronRight,
+  PreviousPage: ChevronLeft,
+  ResetSearch: Clear,
+  Search: Search,
+  SortArrow: ArrowUpward,
+  ThirdStateCheck: Remove,
+  ViewColumn: ViewColumn
+};
+
+
+
 
 class Tables extends React.Component {
   state = {
     groups: [],
     ids: [],
+    columns: [
+      { title: 'Nome', field: 'name' },
+      { title: 'Descrição', field: 'description' },
+      {
+        title: 'Rotina',
+        field: 'rule',
+        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+      },
+    ],
   }
 
   componentDidMount() {
@@ -21,18 +70,8 @@ class Tables extends React.Component {
     })
       .then(response => response.data)
       .then(data => {
-        let ids = [];
-        let groups = data.reduce((acumulador, item) => {
-          ids.push(item.id_group);
-          delete item.id_group;
-          return acumulador.concat([Object.values(item).map(item => item)]);
-        }, []);
-
-        console.log(ids);
-
         this.setState({ 
-          groups:  groups,
-          ids: ids,
+          data:  data,
         });
       });
   }
@@ -42,18 +81,69 @@ class Tables extends React.Component {
       <PageTitle title="Monitoramento" />
       <Grid container spacing={32}>
         <Grid item xs={12}>
-          <MUIDataTable
+          <MaterialTable
+            icons={tableIcons}
             title="Lista de grupos"
-            data={this.state.groups}
-            columns={["Nome", "Descrição", "Rotina"]} // name, description, rule
-            options={{
-              filter: true,
-              filterType: "dropdown",
-              responsive: "scroll",
-              customToolbarSelect: selectedRows => (
-                <TableToolbarSelect selectedRows={selectedRows} ids={this.state.ids} />
-              )
+            columns={this.state.columns}
+            data={this.state.data}
+            editable={{
+              onRowAdd: newData =>
+                new Promise(resolve => {
+                  console.log(newData);
+                  setTimeout(() => {
+                    resolve();
+                    const data = [...this.state.data];
+                    data.push(newData);
+                    this.setState({ ...this.state, data });
+                  }, 600);
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise(resolve => {
+                  console.log(newData);
+                  setTimeout(() => {
+                    resolve();
+                    const data = [...this.state.data];
+                    data[data.indexOf(oldData)] = newData;
+                    this.setState({ ...this.state, data });
+                  }, 600);
+                }),
+              onRowDelete: oldData =>
+                new Promise(resolve => {
+                  console.log(oldData);
+                  let id = oldData.id_group; 
+
+                  axios.delete(`https://za7gskmdj6.execute-api.us-east-1.amazonaws.com/dev/monitoring?groupId=${id}`, {
+                    headers: {
+                      'Content-type': 'aplication/json',
+                      'x-api-key': process.env.REACT_APP_X_API_KEY,
+                    },
+                  })
+                    .then(response => response.data)
+                    .then(result => {
+                      console.log(result);
+                      setTimeout(() => {
+                        resolve();
+                        const data = [...this.state.data];
+                        data.splice(data.indexOf(oldData), 1);
+                        this.setState({ ...this.state, data });
+                      }, 600);
+                    });
+                }),
             }}
+            localization={{
+              toolbar: {
+                searchPlaceholder: 'Busca',
+              },
+              body: {
+                  editRow: {
+                    deleteText: 'Deseja excluir definitivamente?'
+                  }
+              },
+              pagination: {
+                labelDisplayedRows: '{from}-{to} de {count}',
+                labelRowsSelect: 'linhas',
+              }
+          }}
           />
         </Grid>
       </Grid>
