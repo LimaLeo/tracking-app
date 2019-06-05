baed2b957d0b25ea1c689e188c755b2c14ab1bd6
Leonardo Lima <leonardotexeiralima@gmail.com>
2019-06-05 07:46:10 -0300

    Wip

--- src/components/Header/HeaderView.js
+++ src/components/Header/HeaderView.js
@@ -6,16 +6,13 @@ import {
   InputBase,
   Menu,
   MenuItem,
-  Fab,
   withStyles
 } from "@material-ui/core";
 import {
   Menu as MenuIcon,
-  MailOutline as MailIcon,
   NotificationsNone as NotificationsIcon,
   Person as AccountIcon,
   Search as SearchIcon,
-  Send as SendIcon,
   ArrowBack as ArrowBackIcon
 } from "@material-ui/icons";
 import { fade } from "@material-ui/core/styles/colorManipulator";
@@ -23,38 +20,6 @@ import classNames from "classnames";
 
 import { Badge, Typography } from "../Wrappers";
 import Notification from "../Notification";
-import UserAvatar from "../UserAvatar";
-
-const messages = [
-  {
-    id: 0,
-    variant: "warning",
-    name: "Jane Hew",
-    message: "Hey! How is it going?",
-    time: "9:32"
-  },
-  {
-    id: 1,
-    variant: "success",
-    name: "Lloyd Brown",
-    message: "Check out my new Dashboard",
-    time: "9:18"
-  },
-  {
-    id: 2,
-    variant: "primary",
-    name: "Mark Winstein",
-    message: "I want rearrange the appointment",
-    time: "9:15"
-  },
-  {
-    id: 3,
-    variant: "secondary",
-    name: "Liana Dutti",
-    message: "Good news from sale department",
-    time: "9:09"
-  }
-];
 
 const notifications = [
   { id: 0, color: "warning", message: "Check out this awesome ticket" },
@@ -143,20 +108,6 @@ const Header = ({ classes, isSidebarOpened, toggleSidebar, ...props }) => (
         </Badge>
       </IconButton>
       <IconButton
-        color="inherit"
-        aria-haspopup="true"
-        aria-controls="mail-menu"
-        onClick={props.openMailMenu}
-        className={classes.headerMenuButton}
-      >
-        <Badge
-          badgeContent={props.isMailsUnread ? messages.length : null}
-          color="secondary"
-        >
-          <MailIcon classes={{ root: classes.headerIcon }} />
-        </Badge>
-      </IconButton>
-      <IconButton
         aria-haspopup="true"
         color="inherit"
         className={classes.headerMenuButton}
@@ -165,59 +116,6 @@ const Header = ({ classes, isSidebarOpened, toggleSidebar, ...props }) => (
       >
         <AccountIcon classes={{ root: classes.headerIcon }} />
       </IconButton>
-      <Menu
-        id="mail-menu"
-        open={Boolean(props.mailMenu)}
-        anchorEl={props.mailMenu}
-        onClose={props.closeMailMenu}
-        MenuListProps={{ className: classes.headerMenuList }}
-        className={classes.headerMenu}
-        classes={{ paper: classes.profileMenu }}
-        disableAutoFocusItem
-      >
-        <div className={classes.profileMenuUser}>
-          <Typography variant="h4" weight="medium">
-            New Messages
-          </Typography>
-          <Typography
-            className={classes.profileMenuLink}
-            component="a"
-            color="secondary"
-          >
-            {messages.length} New Messages
-          </Typography>
-        </div>
-        {messages.map(message => (
-          <MenuItem key={message.id} className={classes.messageNotification}>
-            <div className={classes.messageNotificationSide}>
-              <UserAvatar color={message.variant} name={message.name} />
-              <Typography size="sm" color="textSecondary">
-                {message.time}
-              </Typography>
-            </div>
-            <div
-              className={classNames(
-                classes.messageNotificationSide,
-                classes.messageNotificationBodySide
-              )}
-            >
-              <Typography weight="medium" gutterBottom>
-                {message.name}
-              </Typography>
-              <Typography color="textSecondary">{message.message}</Typography>
-            </div>
-          </MenuItem>
-        ))}
-        <Fab
-          variant="extended"
-          color="primary"
-          aria-label="Add"
-          className={classes.sendMessageButton}
-        >
-          Send New Message
-          <SendIcon className={classes.sendButtonIcon} />
-        </Fab>
-      </Menu>
       <Menu
         id="notifications-menu"
         open={Boolean(props.notificationsMenu)}
