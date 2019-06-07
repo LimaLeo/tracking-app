471a53ecd1002f8643aded801e1ec4fbd4b3d8f4
Leonardo Lima <leonardotexeiralima@gmail.com>
2019-06-07 07:18:31 -0300

    Added chart dependence in dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,346 +1,12 @@
 import React from "react";
 import {
   Grid,
-  LinearProgress,
-  Select,
-  OutlinedInput,
-  MenuItem,
   withStyles,
 } from "@material-ui/core";
-import {
-  ResponsiveContainer,
-  ComposedChart,
-  Line,
-  Area,
-  YAxis,
-  XAxis
-} from "recharts";
+import Chart from 'react-apexcharts'
+import axios from 'axios';
 
-import mock from "./mock";
-import Widget from "../../components/Widget";
 import PageTitle from "../../components/PageTitle";
-import { Typography } from "../../components/Wrappers";
-import Dot from "../../components/Sidebar/components/Dot";
-
-const getRandomData = (length, min, max, multiplier = 10, maxDiff = 10) => {
-  const array = new Array(length).fill();
-  let lastValue;
-
-  return array.map((item, index) => {
-    let randomValue = Math.floor(Math.random() * multiplier + 1);
-
-    while (
-      randomValue <= min ||
-      randomValue >= max ||
-      (lastValue && randomValue - lastValue > maxDiff)
-    ) {
-      randomValue = Math.floor(Math.random() * multiplier + 1);
-    }
-
-    lastValue = randomValue;
-
-    return { value: randomValue };
-  });
-};
-
-const getMainChartData = () => {
-  const resultArray = [];
-  const tablet = getRandomData(31, 3500, 6500, 7500, 1000);
-  const desktop = getRandomData(31, 1500, 7500, 7500, 1500);
-  const mobile = getRandomData(36, 4500, 15000, 8500, 2500);
-
-  for (let i = 0; i < tablet.length; i++) {
-    resultArray.push({
-      tablet: tablet[i].value,
-      desktop: desktop[i].value,
-      mobile: mobile[i].value
-    });
-  }
-
-  return resultArray;
-};
-
-const mainChartData = getMainChartData();
-
-const PieChartData = [
-  { name: "Group A", value: 400, color: "primary" },
-  { name: "Group B", value: 300, color: "secondary" },
-  { name: "Group C", value: 300, color: "warning" },
-  { name: "Group D", value: 200, color: "success" }
-];
-
-const Dashboard = ({ classes, theme, ...props }) => {
-  return (
-    <React.Fragment>
-      <PageTitle title="Dashboard" button="Latest Reports" />
-      <Grid container spacing={32}>
-        <Grid item xs={4}>
-          <Widget
-            bodyClass={classes.mainChartBody}
-            header={
-              <div className={classes.mainChartHeader}>
-                <Typography variant="headline" color="textSecondary">
-                  Daily Line Chart
-                </Typography>
-                <div className={classes.mainChartHeaderLabels}>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="warning" />
-                    <Typography className={classes.mainChartLegentElement}>Tablet</Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>Mobile</Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>Desktop</Typography>
-                  </div>
-                </div>
-                <Select
-                  value={props.mainChartState}
-                  onChange={e => props.setMainChartState(e.target.value)}
-                  input={
-                    <OutlinedInput
-                      labelWidth={0}
-                      classes={{ notchedOutline: classes.mainChartSelectRoot, input: classes.mainChartSelect }}
-                    />
-                  }
-                  autoWidth
-                >
-                  <MenuItem value="daily">Daily</MenuItem>
-                  <MenuItem value="weekly">Weekly</MenuItem>
-                  <MenuItem value="monthly">Monthly</MenuItem>
-                </Select>
-              </div>
-            }
-          >
-            <ResponsiveContainer width="100%" minWidth={500} height={350}>
-              <ComposedChart
-                margin={{ top: 0, right: -15, left: -15, bottom: 0 }}
-                data={mainChartData}
-              >
-                <YAxis
-                  ticks={[0, 2500, 5000, 7500]}
-                  tick={{ fill: theme.palette.text.hint + '80', fontSize: 14 }}
-                  stroke={theme.palette.text.hint  + '80'}
-                  tickLine={false}
-                />
-                <XAxis
-                  tickFormatter={i => i + 1}
-                  tick={{ fill: theme.palette.text.hint  + '80', fontSize: 14 }}
-                  stroke={theme.palette.text.hint  + '80'}
-                  tickLine={false}
-                />
-                <Area
-                  type="natural"
-                  dataKey="desktop"
-                  fill={theme.palette.background.light}
-                  strokeWidth={0}
-                  activeDot={false}
-                />
-                <Line
-                  type="natural"
-                  dataKey="mobile"
-                  stroke={theme.palette.primary.main}
-                  strokeWidth={2}
-                  dot={false}
-                  activeDot={false}
-                />
-                <Line
-                  type="linear"
-                  dataKey="tablet"
-                  stroke={theme.palette.warning.main}
-                  strokeWidth={2}
-                  dot={{
-                    stroke: theme.palette.warning.dark,
-                    strokeWidth: 2,
-                    fill: theme.palette.warning.main
-                  }}
-                />
-              </ComposedChart>
-            </ResponsiveContainer>
-          </Widget>
-        </Grid>
-
-        <Grid item xs={4}>
-          <Widget
-            bodyClass={classes.mainChartBody}
-            header={
-              <div className={classes.mainChartHeader}>
-                <Typography variant="headline" color="textSecondary">
-                  Daily Line Chart
-                </Typography>
-                <div className={classes.mainChartHeaderLabels}>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="warning" />
-                    <Typography className={classes.mainChartLegentElement}>Tablet</Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>Mobile</Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>Desktop</Typography>
-                  </div>
-                </div>
-                <Select
-                  value={props.mainChartState}
-                  onChange={e => props.setMainChartState(e.target.value)}
-                  input={
-                    <OutlinedInput
-                      labelWidth={0}
-                      classes={{ notchedOutline: classes.mainChartSelectRoot, input: classes.mainChartSelect }}
-                    />
-                  }
-                  autoWidth
-                >
-                  <MenuItem value="daily">Daily</MenuItem>
-                  <MenuItem value="weekly">Weekly</MenuItem>
-                  <MenuItem value="monthly">Monthly</MenuItem>
-                </Select>
-              </div>
-            }
-          >
-            <ResponsiveContainer width="100%" minWidth={500} height={350}>
-              <ComposedChart
-                margin={{ top: 0, right: -15, left: -15, bottom: 0 }}
-                data={mainChartData}
-              >
-                <YAxis
-                  ticks={[0, 2500, 5000, 7500]}
-                  tick={{ fill: theme.palette.text.hint + '80', fontSize: 14 }}
-                  stroke={theme.palette.text.hint  + '80'}
-                  tickLine={false}
-                />
-                <XAxis
-                  tickFormatter={i => i + 1}
-                  tick={{ fill: theme.palette.text.hint  + '80', fontSize: 14 }}
-                  stroke={theme.palette.text.hint  + '80'}
-                  tickLine={false}
-                />
-                <Area
-                  type="natural"
-                  dataKey="desktop"
-                  fill={theme.palette.background.light}
-                  strokeWidth={0}
-                  activeDot={false}
-                />
-                <Line
-                  type="natural"
-                  dataKey="mobile"
-                  stroke={theme.palette.primary.main}
-                  strokeWidth={2}
-                  dot={false}
-                  activeDot={false}
-                />
-                <Line
-                  type="linear"
-                  dataKey="tablet"
-                  stroke={theme.palette.warning.main}
-                  strokeWidth={2}
-                  dot={{
-                    stroke: theme.palette.warning.dark,
-                    strokeWidth: 2,
-                    fill: theme.palette.warning.main
-                  }}
-                />
-              </ComposedChart>
-            </ResponsiveContainer>
-          </Widget>
-        </Grid>
-
-        <Grid item xs={4}>
-          <Widget
-            bodyClass={classes.mainChartBody}
-            header={
-              <div className={classes.mainChartHeader}>
-                <Typography variant="headline" color="textSecondary">
-                  Daily Line Chart
-                </Typography>
-                <div className={classes.mainChartHeaderLabels}>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="warning" />
-                    <Typography className={classes.mainChartLegentElement}>Tablet</Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>Mobile</Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>Desktop</Typography>
-                  </div>
-                </div>
-                <Select
-                  value={props.mainChartState}
-                  onChange={e => props.setMainChartState(e.target.value)}
-                  input={
-                    <OutlinedInput
-                      labelWidth={0}
-                      classes={{ notchedOutline: classes.mainChartSelectRoot, input: classes.mainChartSelect }}
-                    />
-                  }
-                  autoWidth
-                >
-                  <MenuItem value="daily">Daily</MenuItem>
-                  <MenuItem value="weekly">Weekly</MenuItem>
-                  <MenuItem value="monthly">Monthly</MenuItem>
-                </Select>
-              </div>
-            }
-          >
-            <ResponsiveContainer width="100%" minWidth={500} height={350}>
-              <ComposedChart
-                margin={{ top: 0, right: -15, left: -15, bottom: 0 }}
-                data={mainChartData}
-              >
-                <YAxis
-                  ticks={[0, 2500, 5000, 7500]}
-                  tick={{ fill: theme.palette.text.hint + '80', fontSize: 14 }}
-                  stroke={theme.palette.text.hint  + '80'}
-                  tickLine={false}
-                />
-                <XAxis
-                  tickFormatter={i => i + 1}
-                  tick={{ fill: theme.palette.text.hint  + '80', fontSize: 14 }}
-                  stroke={theme.palette.text.hint  + '80'}
-                  tickLine={false}
-                />
-                <Area
-                  type="natural"
-                  dataKey="desktop"
-                  fill={theme.palette.background.light}
-                  strokeWidth={0}
-                  activeDot={false}
-                />
-                <Line
-                  type="natural"
-                  dataKey="mobile"
-                  stroke={theme.palette.primary.main}
-                  strokeWidth={2}
-                  dot={false}
-                  activeDot={false}
-                />
-                <Line
-                  type="linear"
-                  dataKey="tablet"
-                  stroke={theme.palette.warning.main}
-                  strokeWidth={2}
-                  dot={{
-                    stroke: theme.palette.warning.dark,
-                    strokeWidth: 2,
-                    fill: theme.palette.warning.main
-                  }}
-                />
-              </ComposedChart>
-            </ResponsiveContainer>
-          </Widget>
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
-};
 
 const styles = theme => ({
   card: {
@@ -455,5 +121,162 @@ const styles = theme => ({
     marginLeft: theme.spacing.unit,
   }
 });
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: [],
+      products: [],
+      chartsOptions: [],
+      chartsSeries: [],
+
+    }
+  }
+
+  getDates(groups) {
+    return groups;
+  }
+
+  addOption(id, title, categories) {
+    return { 
+      chart: {
+        id: id,
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        width: [5, 7, 5],
+        curve: 'straight',
+        dashArray: [0, 8, 5]
+      },
+      title: {
+        text: title,
+        align: 'left'
+      },
+      markers: {
+        size: 0,
+        hover: {
+          sizeOffset: 6
+        }
+      },
+      xaxis: {
+        categories: categories,
+      },
+      grid: {
+        borderColor: '#f1f1f1',
+      }
+    }
+  }
+
+  getCategories(data) {
+    return data
+      .reduce((dates, item) => {
+                dates.push(item.date)
+                return dates
+          }, 
+      [])
+      .filter((item, index, array) => {
+          return array.indexOf(item) === index;
+      });
+  }
+
+  getSeries(data) {
+    let ids = data.reduce((previous, item) => {
+      previous[item.id_product] = item.id_product;
+      return previous;
+    }, {});
+    
+    ids = Object.keys(ids).map(item => {
+      return item;
+    });
+
+    let result = ids
+      .reduce((previous, item) => {
+        let products = data
+          .filter(product => product.id_product == item)
+          .reduce((previous, item, index) => {
+            previous["name"] = (new URL(item.link)).origin
+            if (index == 0) previous["data"] = []
+            previous["data"].push(item.value);
+            return previous;
+          }, {})
+
+        previous.push(products);
+        return previous;
+      }, []);
+
+    return result;
+  } 
+
+  async componentDidMount() {
+    await axios.get('https://za7gskmdj6.execute-api.us-east-1.amazonaws.com/dev/monitoring/list?userId=1', {
+      headers: {
+        'Content-type': 'aplication/json',
+        'x-api-key': process.env.REACT_APP_X_API_KEY,
+      },
+    })
+      .then(response => response.data)
+      .then(groups => {
+        this.setState({
+          groups: groups,
+        });
+      });
+
+    let groups = this.state.groups;
+
+    groups.map(item => {
+      axios.get(`https://za7gskmdj6.execute-api.us-east-1.amazonaws.com/dev/prices/monitoring?groupId=${item.id_group}`, {
+        headers: {
+          'Content-type': 'aplication/json',
+          'x-api-key': process.env.REACT_APP_X_API_KEY,
+        },
+      })
+        .then(response => response.data)
+        .then(data => {
+          console.log(data);
+          if (data.length) {
+            let id = data[0].id_group || undefined; 
+            let title = data[0].description;
+            let categories = this.getCategories(data);
+            let option = this.addOption(id, title, categories);
+            let result = this.getSeries(data);
+            
+            console.log(option);
+  
+            let products = this.state.products;
+            console.log(result)
+            products.push(result);
+  
+            this.setState({
+              products: products,
+              chartsOptions: [...this.state.chartsOptions, option],
+              chartsSeries: [...this.state.chartsSeries, result],
+            });
+            console.log(this.state.chartsOptions);
+            console.log(this.state.chartsSeries);
+          } 
+        });
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <PageTitle title="Dashboard" button="Latest Reports" />
+        <Grid container spacing={32}>
+          {
+            this.state.chartsOptions.map((options, i) => {
+              return <Grid item xs={4} key={options.chart.id}>
+                      <Chart options={options} series={this.state.chartsSeries[i]} type="line" />
+                    </Grid>
+            })
+          }
+        </Grid>
+      </React.Fragment>
+    )
+  };
+};
 
 export default withStyles(styles, { withTheme: true })(Dashboard);
